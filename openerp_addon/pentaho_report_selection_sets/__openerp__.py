fc2f56766a8834b714119a16ba72177b4bd04959
Richard deMeester <richard@willowit.com.au>
2014-04-23 16:14:31 +1000

    Report selection set security added.

--- openerp_addon/pentaho_report_selection_sets/__openerp__.py
+++ openerp_addon/pentaho_report_selection_sets/__openerp__.py
@@ -8,6 +8,8 @@ Pentaho - Report Selections Saving
 This module builds on the OpenERP Pentaho Report functionality by allowing report selections to be stored and
 retrieved.  Those selections can have a dynamic element by using selection default functions.
 
+To be able to store and maintain selection sets, a user must have their security set accordingly.
+
     """,
     "version": "0.1",
     "author": "WillowIT Pty Ltd",
@@ -15,6 +17,7 @@ retrieved.  Those selections can have a dynamic element by using selection defau
     "depends": ["pentaho_reports"],
     "category": "Reporting subsystems",
     "data": [
+             "security/pentaho_selection_set_security.xml",
              "security/ir.model.access.csv",
              "wizard/store_selections.xml",
              "report_prompt.xml",
